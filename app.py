a312634d6c9ae7a1fc04bc7617bfe62b3c862fab
Лена Минько <minkolena2005@mail.ru>
2025-09-18 17:10:39 +0700

    Информация об авторе

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def start(): 
+@app.route("/web")
+def web(): 
     return """<!doctype html> 
         <html>
             <body>
-                <h1>web-сервер на flask</h1> 
+                <h1>web-сервер на flask</h1>
+                <a href="/author">author</a>
             </body> 
         </html>"""
+
+@app.route("/author")
+def author():
+    name = "Минько Елена Михайловна"
+    group = "ФБИ-34"
+    faculty = "ФБ"
+
+    return """<!doctype html>
+        <html>
+            <body>
+                <p>Студент: """ + name + """ </p>
+                <p>Группа: """ + group + """ </p>
+                <p>Факультет: """ + faculty + """ </p>
+                <a href="/web">web</a>
+            </body>
+        </html>""" 
