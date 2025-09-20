669e153dfd8300def404db08b8d57f5f81d83b40
Елена Минько <minkolena2005@mail.ru>
2025-09-20 10:50:18 +0700

    Имя студента

--- app.py
+++ app.py
@@ -419,5 +419,6 @@ def add_flower(name):
 ''' 
 @app.route('/lab2/example')
 def example():
-    return render_template('example.html')
+    name = 'Елена Минько'
+    return render_template('example.html', name=name)
     
