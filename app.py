fbe55dfbd42a5e18da7641b1e71da85716a9aee1
Елена Минько <minkolena2005@mail.ru>
2025-09-20 10:57:37 +0700

    Передача переменных в шаблон

--- app.py
+++ app.py
@@ -420,5 +420,11 @@ def add_flower(name):
 @app.route('/lab2/example')
 def example():
     name = 'Елена Минько'
-    return render_template('example.html', name=name)
-    
+    lab_number = 2
+    group = 'ФБИ-34'
+    course = 3
+    return render_template('example.html', 
+                         name=name, 
+                         lab_number=lab_number, 
+                         group=group, 
+                         course=course)
