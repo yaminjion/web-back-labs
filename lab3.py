a0f6e076692b1c8dfc4f013296bb2cb2f06e300b
Лена Минько <minkolena2005@mail.ru>
2025-10-07 18:42:29 +0700

    Валидация

--- lab3.py
+++ lab3.py
@@ -28,7 +28,12 @@ def del_cookie():
 
 @lab3.route('/lab3/form1')
 def form1():
+    errors = {}
     user = request.args.get('user')
+    if user == '':
+        errors['user']='Заполните поле!'
     age = request.args.get('age')
+    if age == '':
+        errors['age'] = 'Заполните поле!'
     sex = request.args.get('sex')
-    return render_template('lab3/form1.html', user=user, age=age, sex=sex)
+    return render_template('lab3/form1.html', user=user, age=age, sex=sex, errors=errors)
