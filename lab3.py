4789ed98e9416977afd4edf2b01e88f99ed27d24
Лена Минько <minkolena2005@mail.ru>
2025-10-10 23:33:50 +0700

    Настройки отображения

--- lab3.py
+++ lab3.py
@@ -84,3 +84,35 @@ def success():
         price += 10
 
     return render_template('lab3/success.html', price=price)
+
+
+@lab3.route('/lab3/settings')
+def settings():
+    color = request.args.get('color')
+    bg_color = request.args.get('bg_color')
+    font_size = request.args.get('font_size')
+    font_style = request.args.get('font_style')
+    
+    if color or bg_color or font_size or font_style:
+        resp = make_response(redirect('/lab3/settings'))
+        if color:
+            resp.set_cookie('color', color)
+        if bg_color:
+            resp.set_cookie('bg_color', bg_color)
+        if font_size:
+            resp.set_cookie('font_size', font_size)
+        if font_style:
+            resp.set_cookie('font_style', font_style)
+        return resp
+
+    color = request.cookies.get('color')
+    bg_color = request.cookies.get('bg_color')
+    font_size = request.cookies.get('font_size')
+    font_style = request.cookies.get('font_style')
+    
+    resp = make_response(render_template('lab3/settings.html', 
+                                        color=color, 
+                                        bg_color=bg_color, 
+                                        font_size=font_size, 
+                                        font_style=font_style))
+    return resp
