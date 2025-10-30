a79398a142f2f4302e83d82a263e2dce32bf2c9e
Лена Минько <minkolena2005@mail.ru>
2025-10-30 15:07:24 +0700

    Авторизация

--- lab4.py
+++ lab4.py
@@ -129,3 +129,18 @@ def tree():
             tree_count += 1
     
     return redirect(url_for('lab4.tree'))
+
+
+@lab4.route('/lab4/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('lab4/login.html', authorized=False)
+
+    login = request.form.get('login')
+    password = request.form.get('password')
+
+    if login == 'alex' and password == '123':
+        return render_template('lab4/login.html', login=login, authorized=True)
+    
+    error = "Неверные логин и/или пароль"
+    return render_template('lab4/login.html', error=error, authorized=False)
