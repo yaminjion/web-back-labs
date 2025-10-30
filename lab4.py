0619771f0b47ccab97bcef5268f9f7c3cde300e7
Лена Минько <minkolena2005@mail.ru>
2025-10-30 11:04:59 +0700

    Счётчик деревьев

--- lab4.py
+++ lab4.py
@@ -1,13 +1,16 @@
 from flask import Blueprint, render_template, request
 lab4 = Blueprint('lab4', __name__)
 
+
 @lab4.route('/lab4/')
 def lab():
     return render_template('lab4/lab4.html')
 
+
 @lab4.route('/lab4/div-form')
 def div_form():
     return render_template('lab4/div-form.html')
+
 
 @lab4.route('/lab4/div', methods = ['POST'])
 def div():
@@ -24,10 +27,12 @@ def div():
     result = x1/x2
     return render_template('lab4/div.html', x1=x1, x2=x2, result=result)
 
+
 # Суммирование
 @lab4.route('/lab4/sum-form')
 def sum_form():
     return render_template('lab4/sum-form.html')
+
 
 @lab4.route('/lab4/sum', methods=['POST'])
 def sum():
@@ -40,10 +45,12 @@ def sum():
     result = x1 + x2
     return render_template('lab4/sum.html', x1=x1, x2=x2, result=result)
 
+
 # Вычитание
 @lab4.route('/lab4/sub-form')
 def sub_form():
     return render_template('lab4/sub-form.html')
+
 
 @lab4.route('/lab4/sub', methods=['POST'])
 def sub():
@@ -57,6 +64,7 @@ def sub():
     x2 = int(x2)
     result = x1 - x2
     return render_template('lab4/sub.html', x1=x1, x2=x2, result=result)
+
 
 # Умножение
 @lab4.route('/lab4/mul-form')
@@ -74,10 +82,12 @@ def mul():
     result = x1 * x2
     return render_template('lab4/mul.html', x1=x1, x2=x2, result=result)
 
+
 # Возведение в степень
 @lab4.route('/lab4/pow-form')
 def pow_form():
     return render_template('lab4/pow-form.html')
+
 
 @lab4.route('/lab4/pow', methods=['POST'])
 def power():
@@ -95,3 +105,21 @@ def power():
     
     result = x1 ** x2
     return render_template('lab4/pow.html', x1=x1, x2=x2, result=result)
+
+
+tree_count = 0
+
+@lab4.route('/lab4/tree', methods=['GET', 'POST'])
+def tree():
+    global tree_count
+    if request.method=='GET':
+        return render_template('lab4/tree.html', tree_count=tree_count)
+    
+    operation = request.form.get('operation')
+
+    if operation == 'cut':
+        tree_count -= 1
+    elif operation == 'plant':
+        tree_count += 1
+
+    return render_template('lab4/tree.html', tree_count=tree_count)
