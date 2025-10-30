d7565babd6c5629c7f959a9635850d63546140f5
Лена Минько <minkolena2005@mail.ru>
2025-10-30 11:20:00 +0700

    POST/Redirect/GET

--- lab4.py
+++ lab4.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 lab4 = Blueprint('lab4', __name__)
 
 
@@ -112,14 +112,15 @@ tree_count = 0
 @lab4.route('/lab4/tree', methods=['GET', 'POST'])
 def tree():
     global tree_count
-    if request.method=='GET':
-        return render_template('lab4/tree.html', tree_count=tree_count)
     
+    if request.method == 'GET':
+        return render_template('lab4/tree.html', tree_count=tree_count)
+
     operation = request.form.get('operation')
 
     if operation == 'cut':
         tree_count -= 1
     elif operation == 'plant':
         tree_count += 1
-
-    return render_template('lab4/tree.html', tree_count=tree_count)
+    
+    return redirect(url_for('lab4.tree'))
