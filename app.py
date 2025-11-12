ff228187d901b26aa659ad99e88d3c24170b3f26
Лена Минько <minkolena2005@mail.ru>
2025-11-13 02:35:02 +0700

    доработка

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 app = Flask(__name__)
-app.secret_key = 'секретно-секретный секрет'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'Секретно-секретный секрет')
+app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
