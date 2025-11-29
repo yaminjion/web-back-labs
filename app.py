7d4f7b615036e5ca97d94ca75810cf0fdd6c313b
Студент Факультета Бизнеса <student@FB.LOCAL>
2025-11-29 09:18:15 +0700

    Заготовка к 7-ой лабораторной

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from lab3 import lab3
 from lab4 import lab4
 from lab5 import lab5
 from lab6 import lab6
+from lab7 import lab7
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'Секретно-секретный секрет')
 app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
@@ -20,7 +21,7 @@ app.register_blueprint(lab3)
 app.register_blueprint(lab4)
 app.register_blueprint(lab5)
 app.register_blueprint(lab6)
-
+app.register_blueprint(lab7)
 count = 0
 
 @app.route("/")
@@ -48,6 +49,7 @@ def index():
                     <li><a href="/lab4/">Четвертая лабораторная</a></li>
                     <li><a href="/lab5/">Пятая лабораторная</a></li>
                     <li><a href="/lab6/">Шестая лабораторная</a></li>
+                    <li><a href="/lab7/">Седьмая лабораторная</a></li>
                 </ul>
             </nav>
         </main>
